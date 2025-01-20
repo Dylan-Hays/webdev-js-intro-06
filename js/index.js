637e8232b839d4ed56ba8ac874c9adec9f14dec1
Ashen-Q <dylanhays849@gmail.com>
2025-01-19 19:43:46 -0500

    My compleated assignment for Java Script intro #6

--- js/index.js
+++ js/index.js
@@ -1,3 +1,39 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+"use strict";
+
+/*
+// For Loop Example
+for (let i = 0; i < 5; i++) {
+    if (i === 3) {
+        console.log("Breaking at", i);
+        break;  // Exiting loop when i equals 3
+    }
+    console.log(i);
+}
+*/
+
+/*
+// While Loop Example
+let j = 0;
+while (j < 5) {
+    j++;
+    if (j === 2) {
+        console.log("Skipping", j);
+        continue;  // Skipping when j equals 2
+    }
+    console.log(j);
+}
+*/
+
+/*
+// Do-While Loop Example
+let k = 0;
+do {
+    console.log(k);
+    k++;
+    if (k === 4) {
+        break;  // Exiting loop at k = 4
+    }
+} while (k < 5);
+*/
